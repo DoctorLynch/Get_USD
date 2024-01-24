5ddc6d96744b22df74fc323435948b842ff4c4cb
DoctorLynch <doctor_linchenko@mail.ru>
2024-01-24 19:19:11 +0300

    It all

--- dollar_rate/views.py
+++ dollar_rate/views.py
@@ -8,21 +8,23 @@ Api_key = os.getenv('API_KEY')
 
 new_dollar_rate = {}
 last_10_values = []
+USD_RUB_API_URL = f'https://api.currencyapi.com/v3/latest?apikey={Api_key}&currencies=RUB'
 
 
 def index(request):
-    for i in range(1, 10):
-        response = requests.get(f'https://api.currencyapi.com/v3/latest?apikey={Api_key}&currencies=RUB')
-        dollar_rate = response.json()
-        new_dollar_rate['Последнее обновление'] = dollar_rate['meta']
-        new_dollar_rate['Курс доллара на сегодня'] = dollar_rate['data']['RUB']['value']
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        new_dollar_rate['Время обновления'] = current_time
-        last_10_values.append([i, new_dollar_rate])
-        context = {
-            'dollar_rate': last_10_values
-        }
-        time.sleep(10)
+    response = requests.get(USD_RUB_API_URL)
+    dollar_rate = response.json()
+    new_dollar_rate['Последнее обновление'] = dollar_rate['meta']
+    new_dollar_rate['Курс доллара на сегодня'] = dollar_rate['data']['RUB']['value']
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    new_dollar_rate['Время обновления'] = current_time
+    last_10_values.append(new_dollar_rate)
+    if len(last_10_values) > 10:
+        last_10_values.pop(0)
+    context = {
+        'dollar_rate': last_10_values
+    }
+    time.sleep(10)
 
     return render(request, 'dollar_rate/index.html', context)
